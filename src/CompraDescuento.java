e1587c483385b483bda55c031c6e4d5e2c8ab877
alrago24 <alinson_04@hotmail.com>
2025-05-10 11:58:38 -0500

    Primera versión del ejercicio de descuento

---
+++ src/CompraDescuento.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CompraDescuento {
+    public static void main(String[] args) {
+
+        Scanner entradaUsuario = new Scanner(System.in);
+
+        System.out.print("Ingresa el valor de la compra: ");
+        double valorCompra = entradaUsuario.nextDouble();
+
+        if (valorCompra > 200000) {
+            double descuento = valorCompra * 0.15;
+            double totalConDescuento = valorCompra - descuento;
+            System.out.println("Se aplicó un 15% de descuento.");
+            System.out.println("Total a pagar: $" + totalConDescuento);
+        } else {
+            System.out.println("No aplica descuento.");
+            System.out.println("Total a pagar: $" + valorCompra);
+        }
+
+        entradaUsuario.close();
+    }
+}
